bff88c9d1e42e2e49239c2b21748f3efc39d879c
Maxime GELLE <maxime.gelle@gmail.com>
2022-02-21 23:27:09 +0100

    Somme corrections

--- inventory/src/test/resources/contracts/shouldRetrieveAllBooks.groovy
+++ inventory/src/test/resources/contracts/shouldRetrieveAllBooks.groovy
@@ -16,12 +16,12 @@ Contract.make {
         [{
             "id": "d4d37e73-77a0-4616-8bd2-5ed983d45d14",
             "name": "Java",
-            "price": "19.9",
+            "price": 19.9,
             "stock": 100
         },{
             "id": "8364948b-6221-4cd8-9fd9-db0d17d45ef8",
             "name": "Kotlin",
-            "price": "22.4",
+            "price": 22.4,
             "stock": 0
         }]
         """)
