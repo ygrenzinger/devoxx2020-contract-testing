5322c79c0b6dab29345b9f6c682685c68f28afaa
Maxime GELLE <maxime.gelle@gmail.com>
2022-03-02 22:44:52 +0100

    Working Contract

---
+++ inventory/src/test/java/com/devoxx/inventory/contracts/ContractVerificationTest.java
@@ -0,0 +1,24 @@
+package com.devoxx.inventory.contracts;
+
+import au.com.dius.pact.provider.junit5.PactVerificationContext;
+import au.com.dius.pact.provider.junitsupport.Provider;
+import au.com.dius.pact.provider.junitsupport.loader.PactBroker;
+import au.com.dius.pact.provider.spring.junit5.PactVerificationSpringProvider;
+import org.junit.jupiter.api.TestTemplate;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@Provider("inventory-service")
+@PactBroker
+public class ContractVerificationTest {
+
+    @TestTemplate
+    @ExtendWith(PactVerificationSpringProvider.class)
+    void pactVerificationTestTemplate(PactVerificationContext context) {
+        context.verifyInteraction();
+    }
+
+}
